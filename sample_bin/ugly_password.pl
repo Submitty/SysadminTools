34108db167534bcb7c6c5f19a26d6398560aa22d
pbailie <pbailie@users.noreply.github.com>
2018-08-07 15:51:26 -0400

    Ugly Password Script Changes to be committed: modified:   sample_bin/ugly_password.pl

--- sample_bin/ugly_password.pl
+++ sample_bin/ugly_password.pl
@@ -1,15 +1,15 @@
-#!/usr/bin/env perl -l
+#!/usr/bin/env perl
 
 # File: ugly_password.pl
-# Date: August 6, 2018
+# Date: August 7, 2018
 # Author: Peter Bailie
 #
 # This will output to STDOUT a randomly generated ugly password using the
 # printable ASCII character range of 33 - 126.  Passwords are based on
 # /dev/random to ensure reliable pseudo-random entropy.
 #
-# /dev/random is blocking, so there may be a delay in output when the seed pool
-# needs to be refreshed.
+# /dev/random is blocking, so there may be a (possibly lengthy) delay in
+# output when the seed pool needs to be refreshed.
 
 use strict;
 use warnings;
@@ -18,15 +18,9 @@ use autodie;
 my $PW_LENGTH = 32;  # length of the ugly password in characters.
 my ($fh, $byte, $val, $output);
 
-$output = "";  # password output
 open $fh, '<:raw', '/dev/random';
-build_ugly_password();
-close $fh;
-print STDOUT $output;
-exit 0;
-
-# Recursive subroutine to generate a random char and build the ugly password.
-sub build_ugly_password {
+$output = "";  # password output
+while (length $output < $PW_LENGTH) {
 	# Read a random byte and scale it to range 33 - 126.
 	read $fh, $byte, 1;
 	$val = (unpack 'C', $byte) % 94 + 33;
@@ -37,9 +31,6 @@ sub build_ugly_password {
 		# Character qualifies, append it to password.
 		$output .= chr $val;
 	}
-
-	# Go again, if needed.
-	if (length $output < $PW_LENGTH) {
-		build_ugly_password();
-	}
 }
+close $fh;
+print STDOUT $output . "\n";
