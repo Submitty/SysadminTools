0ada5d34c440329f96dc572e6274c8b44dc76cbc
pbailie <pbailie@users.noreply.github.com>
2019-06-14 20:56:26 -0400

    Update preferred_name_logging.php
    Psql can generate several logfiles that need to be parsed.

--- preferred_name_logging/preferred_name_logging.php
+++ preferred_name_logging/preferred_name_logging.php
@@ -7,11 +7,8 @@ class main {
         'postgresql_logfile_path' => "/var/log/postgresql/",
         'submitty_logfile_path'   => "/var/log/postgresql/",
         'postgresql_logfile'      => "postgresql",
-        'submitty_logfile'        => "preferred_names_log"
+        'submitty_logfile'        => "submitty_preferred_names"
     );
-
-    private static $psql_logfile;
-    private static $submitty_logfile;
 
     public static function run() {
         // make sure we are running as root from cli
@@ -21,81 +18,84 @@ class main {
             exit("This is a command line script.  Root required." . PHP_EOL);
         }
 
-        self::config();
         self::parse_and_write_logs();
 
         exit(0);
     }
 
-    private static function config() {
-        self::$psql_logfile = CONFIG['psql_logfile_path'] . CONFIG['psql_logfile'];
-        self::$submitty_logfile = CONFIG['submitty_logfile_path'] . CONFIG['submitty_logfile'];
-    }
-
     private static function parse_and_write_logs() {
-        $psql_fh = fopen(self::$psql_logfile, 'r');
-        if ($psql_fh === false) {
-            exit("Cannot open Postgresql logfile" . PHP_EOL);
-        }
-
-        $submitty_fh = fopen(self::$submitty_logfile, 'w')
+        $submitty_logfile = self::CONFIG['submitty_logfile_path'] . self::CONFIG['submitty_logfile'] . "_" . date("m-d-y") . "log";
+        $submitty_fh = fopen($submitty_logfile, 'w')
         if ($submitty_fh === false) {
-            exit("Cannot open Submitty logfile". PHP_EOL);
+            exit("Cannot create Submitty logfile". PHP_EOL);
         }
 
-        $line = fgets($psql_fh);
-        while($line !== false) {
-            $row = str_getcsv($line);
-            switch(true) {
-            case $row[7] !== "UPDATE":
-            case $row[11] !== "LOG":
-            case $row[13] !== "PREFERRED_NAME DATA UPDATE":
-                $line = fgets($psql_fh);
+        $preg_str = "~^" . self::CONFIG['postgresql_logfile'] . "\-" . preg_quote(date("Y-m-d", time() - 86400)) . "_\d{6}\.csv$~";
+        $logfiles = scandir(self::CONFIG['postgresql_logfile_path']);
+        $logfiles = preg_grep($preg_str, $logfiles);
+
+        foreach ($logfiles as $logfile) {
+            $psql_fh = fopen(CONFIG::['postgresql_logfile_path'] . $logfile, 'r');
+            if ($psql_fh === false) {
+                printf(STDERR, "Cannot open {$logfile}%s", PHP_EOL);
                 continue;
             }
 
-            $date = $row[8] . "  ";
-
-            if (preg_match("~/\* AUTH: [\w\-]+ \*/~", $row[18]) === 1) {
-                $key['start'] = strpos($row[18], "/* AUTH: ") + 3;
-                $key['end'] = strpos($row[18], " */");
-                $auth = " | " . substr($row[18], $key['start'], $key['end'] - $key['start']);
-            } else {
-                $auth = " | AUTH NOT LOGGED";
-            }
-
-            $preferred_names = array();
-            $preferred_names_data = explode(" ", $row[14]);
-            $key = array_search("PREFERRED_FIRSTNAME", $preferred_names_data);
-            if ($key !== false) {
-                $preferred_name['first']['old'] = $preferred_names_data[$key+2];
-                $preferred_name['first']['new'] = $preferred_names_data[$key+4];
-            }
-
-            $key = array_search("PREFERRED_LASTNAME", $preferred_names_data);
-            if ($key !== false) {
-                $preferred_name['last']['old'] = $preferred_names_data[$key+2];
-                $preferred_name['last']['new'] = $preferred_names_data[$key+4];
-            }
-
-            $log = $date . $auth;
-            if (isset($preferred_name['first'])) {
-                $log .= " | PREFERRED_FIRSTNAME OLD: {$preferred_name['first']['old']} -> NEW: {$preferred_name['first']['new']}";
-            } else {
-                $log .= " | PREFERRED_FIRSTNAME UNCHANGED";
-            }
-
-            if (isset($preferred_name['last'])) {
-                $log .= " | PREFERRED_LASTNAME OLD: {$preferred_name['last']['old']} -> NEW: {$preferred_name['last']['new']}";
-            } else {
-                $log .= " | PREFERRED_LASTNAME UNCHANGED";
-            }
-
-            fwrite($submitty_fh, $log . PHP_EOL);
             $line = fgets($psql_fh);
+            while($line !== false) {
+                $row = str_getcsv($line);
+                switch(true) {
+                case $row[7] !== "UPDATE":
+                case $row[11] !== "LOG":
+                case $row[13] !== "PREFERRED_NAME DATA UPDATE":
+                    $line = fgets($psql_fh);
+                    continue;
+                }
+
+                $date = $row[8] . "  ";
+
+                if (preg_match("~/\* AUTH: [\w\-]+ \*/~", $row[18]) === 1) {
+                    $key['start'] = strpos($row[18], "/* AUTH: ") + 3;
+                    $key['end'] = strpos($row[18], " */");
+                    $auth = " | " . substr($row[18], $key['start'], $key['end'] - $key['start']);
+                } else {
+                    $auth = " | AUTH NOT LOGGED";
+                }
+
+                $preferred_name = array();
+                $preferred_names_data = explode(" ", $row[14]);
+                $key = array_search("PREFERRED_FIRSTNAME", $preferred_names_data);
+                if ($key !== false) {
+                    $preferred_name['first']['old'] = $preferred_names_data[$key+2];
+                    $preferred_name['first']['new'] = $preferred_names_data[$key+4];
+                }
+
+                $key = array_search("PREFERRED_LASTNAME", $preferred_names_data);
+                if ($key !== false) {
+                    $preferred_name['last']['old'] = $preferred_names_data[$key+2];
+                    $preferred_name['last']['new'] = $preferred_names_data[$key+4];
+                }
+
+                $log = $date . $auth;
+                if (isset($preferred_name['first'])) {
+                    $log .= " | PREFERRED_FIRSTNAME OLD: {$preferred_name['first']['old']} -> NEW: {$preferred_name['first']['new']}";
+                } else {
+                    $log .= " | PREFERRED_FIRSTNAME UNCHANGED";
+                }
+
+                if (isset($preferred_name['last'])) {
+                    $log .= " | PREFERRED_LASTNAME OLD: {$preferred_name['last']['old']} -> NEW: {$preferred_name['last']['new']}";
+                } else {
+                    $log .= " | PREFERRED_LASTNAME UNCHANGED";
+                }
+
+                fwrite($submitty_fh, $log . PHP_EOL);
+                $line = fgets($psql_fh);
+            }
+
+            fclose($psql_fh);
         }
 
-        fclose($psql_fh);
         fclose($submitty_fh);
     }
 }
