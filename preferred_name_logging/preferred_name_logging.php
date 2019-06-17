159f4e4303eaa3118c9ea312f56b9c1b167c00cc
pbailie <pbailie@users.noreply.github.com>
2019-06-17 18:07:24 -0400

    Update preferred_name_logging.php
    WIP

--- preferred_name_logging/preferred_name_logging.php
+++ preferred_name_logging/preferred_name_logging.php
@@ -7,14 +7,15 @@ class main {
         'postgresql_logfile_path' => "/var/log/postgresql/",
         'submitty_logfile_path'   => "/var/log/postgresql/",
         'postgresql_logfile'      => "postgresql",
-        'submitty_logfile'        => "submitty_preferred_names"
+        'submitty_logfile'        => "submitty_preferred_names",
+        'postgresql_log_row_count'    => 23
     );
 
     public static function run() {
         // make sure we are running as root from cli
         switch (true) {
         case posix_getuid() !== 0:
-        case php_sapi !== 'cli':
+        case PHP_SAPI !== 'cli':
             exit("This is a command line script.  Root required." . PHP_EOL);
         }
 
@@ -24,46 +25,49 @@ class main {
     }
 
     private static function parse_and_write_logs() {
-        $submitty_logfile = self::CONFIG['submitty_logfile_path'] . self::CONFIG['submitty_logfile'] . "_" . date("m-d-y") . "log";
-        $submitty_fh = fopen($submitty_logfile, 'w')
+        $submitty_logfile = sprintf("%s%s_%s.log", self::CONFIG['submitty_logfile_path'], self::CONFIG['submitty_logfile'], date("m-d-y"));
+        $submitty_fh = fopen($submitty_logfile, 'w');
         if ($submitty_fh === false) {
             exit("Cannot create Submitty logfile". PHP_EOL);
         }
 
-        $preg_str = "~^" . self::CONFIG['postgresql_logfile'] . "\-" . preg_quote(date("Y-m-d", time() - 86400)) . "_\d{6}\.csv$~";
+        $preg_str = sprintf("~^%s\-%s_\d{6}\.csv$~", self::CONFIG['postgresql_logfile'], preg_quote(date("Y-m-d", time() - 86400 )));
         $logfiles = scandir(self::CONFIG['postgresql_logfile_path']);
         $logfiles = preg_grep($preg_str, $logfiles);
 
         foreach ($logfiles as $logfile) {
-            $psql_fh = fopen(CONFIG::['postgresql_logfile_path'] . $logfile, 'r');
+            $psql_fh = fopen(self::CONFIG['postgresql_logfile_path'] . $logfile, 'r');
             if ($psql_fh === false) {
-                printf(STDERR, "Cannot open {$logfile}%s", PHP_EOL);
+                fprintf(STDERR, "Cannot open {$logfile}%s", PHP_EOL);
                 continue;
             }
 
-            $line = fgets($psql_fh);
-            while($line !== false) {
-                $row = str_getcsv($line);
+            $psql_row = fgetcsv($psql_fh);
+            $psql_row_num = 1;
+            while($psql_row !== false) {
                 switch(true) {
-                case $row[7] !== "UPDATE":
-                case $row[11] !== "LOG":
-                case $row[13] !== "PREFERRED_NAME DATA UPDATE":
-                    $line = fgets($psql_fh);
+                case count($psql_row) !== self::CONFIG['postgresql_log_row_count']:
+                    fprintf(STDERR, "NOTICE: PSQL log row %d had %d columns.  %d columns expected.  Row ignored.%s", $psql_row_num, count($psql_row), self::CONFIG['postgresql_log_row_count'], PHP_EOL);
+                case $psql_row[7] !== "UPDATE":
+                case $psql_row[11] !== "LOG":
+                case $psql_row[13] !== "PREFERRED_NAME DATA UPDATE":
+                    $psql_row = fgetcsv($psql_fh);
+                    $psql_row_num++;
                     continue;
                 }
 
-                $date = $row[8] . "  ";
+                $date = $psql_row[8] . "  ";
 
-                if (preg_match("~/\* AUTH: [\w\-]+ \*/~", $row[18]) === 1) {
-                    $key['start'] = strpos($row[18], "/* AUTH: ") + 3;
-                    $key['end'] = strpos($row[18], " */");
-                    $auth = " | " . substr($row[18], $key['start'], $key['end'] - $key['start']);
+                if (preg_match("~/\* AUTH: [\w\-]+ \*/~", $psql_row[18]) === 1) {
+                    $key['start'] = strpos($psql_row[18], "/* AUTH: ") + 3;
+                    $key['end'] = strpos($psql_row[18], " */");
+                    $auth = " | " . substr($psql_row[18], $key['start'], $key['end'] - $key['start']);
                 } else {
                     $auth = " | AUTH NOT LOGGED";
                 }
 
                 $preferred_name = array();
-                $preferred_names_data = explode(" ", $row[14]);
+                $preferred_names_data = explode(" ", $psql_row[14]);
                 $key = array_search("PREFERRED_FIRSTNAME", $preferred_names_data);
                 if ($key !== false) {
                     $preferred_name['first']['old'] = $preferred_names_data[$key+2];
@@ -90,7 +94,8 @@ class main {
                 }
 
                 fwrite($submitty_fh, $log . PHP_EOL);
-                $line = fgets($psql_fh);
+                $psql_row = fgetcsv($psql_fh);
+                $psql_row_num++;
             }
 
             fclose($psql_fh);
