ebbc695c30f3fdfb584527034ef1bce7aa00cffb
pbailie <pbailie@users.noreply.github.com>
2019-07-19 20:06:24 -0400

    Update preferred_name_logging.php
    PR Candidate

--- preferred_name_logging/preferred_name_logging.php
+++ preferred_name_logging/preferred_name_logging.php
@@ -25,18 +25,18 @@ class main {
         'submitty_logfile_path'    => "/var/log/submitty/",
         'postgresql_logfile'       => "postgresql",
         'submitty_logfile'         => "submitty_preferred_names",
-        'postgresql_log_row_count' => 23,
         'submitty_log_retention'   => 7
     );
 
-    /**
-     * Number of days of preferred_name_change logs to keep.
-     *
-     * @var int
-     * @access private
-     * @static
-     */
-    private static $log_retention_days;
+    private const CONSTANTS = array(
+        'postgresql_log_row_count' => 23,
+        'psql_validation_update'   => 7,
+        'psql_validation_log'      => 11,
+        'psql_validation_pfn'      => 13,
+        'psql_data_date'           => 8,
+        'psql_data_auth'           => 19,
+        'psql_data_pfn'            => 14
+    );
 
     /**
      * Method to invoke to run this program: main::run()
@@ -68,6 +68,14 @@ class main {
      * @static
      */
     private static function parse_and_write_logs() {
+        //Check to make sure Submitty log directory path exists.  Create it if needed.
+        if (file_exists(self::CONFIG['submitty_logfile_path']) === false) {
+            if (mkdir(self::CONFIG['submitty_logfile_path'], 0700) === false) {
+                self::log("Submitty log folder needed, mkdir failed.");
+                exit(1);
+            }
+        }
+
         //Prepare submitty preferred name change log file for today.
         $submitty_logfile = sprintf("%s%s_%s.log", self::CONFIG['submitty_logfile_path'], self::CONFIG['submitty_logfile'], date("Y-m-d"));
         $submitty_fh = fopen($submitty_logfile, 'w');
@@ -79,7 +87,7 @@ class main {
         }
 
         //There can be multiple psql log files that need to be read.
-        $preg_str = sprintf("~^%s\-%s_\d{6}\.csv$~", self::CONFIG['postgresql_logfile'], preg_quote(date("Y-m-d", time() - 86400 )));
+        $preg_str = sprintf("~^%s\-%s_\d{6}\.csv$~", self::CONFIG['postgresql_logfile'], preg_quote(date("Y-m-d", time() - 86400)));
         $logfiles = preg_grep($preg_str, scandir(self::CONFIG['postgresql_logfile_path']));
 
         foreach ($logfiles as $logfile) {
@@ -94,32 +102,36 @@ class main {
             while($psql_row !== false) {
                 //Validation.  Any case is true, validation fails and row is ignored.
                 switch(true) {
-                case count($psql_row) !== self::CONFIG['postgresql_log_row_count']:
+                case count($psql_row) !== self::CONSTANTS['postgresql_log_row_count']:
                     self::log(sprintf("NOTICE: PSQL log row %d had %d columns.  %d columns expected.  Row ignored.%s", $psql_row_num, count($psql_row), self::CONFIG['postgresql_log_row_count']));
-                case $psql_row[7] !== "UPDATE":
-                case $psql_row[11] !== "LOG":
-                case $psql_row[13] !== "PREFERRED_NAME DATA UPDATE":
+                case $psql_row[self::CONSTANTS['psql_validation_update']] !== "UPDATE":
+                case $psql_row[self::CONSTANTS['psql_validation_log']] !== "LOG":
+                case $psql_row[self::CONSTANTS['psql_validation_pfn']] !== "PREFERRED_NAME DATA UPDATE":
                     $psql_row = fgetcsv($psql_fh);
                     $psql_row_num++;
-                    continue;
+                    continue 2;
                 }
 
                 //Validation successful, process row.
+                //Trim all values in the row
+                $psql_row = array_map('trim', $psql_row);
+
                 //Get date token.
-                $date = $psql_row[8] . "  ";
+                $date = $psql_row[self::CONSTANTS['psql_data_date']];
 
                 //Get "AUTH" token (who logged the change).
-                if (preg_match("~/\* AUTH: [\w\-]+ \*/~", $psql_row[18]) === 1) {
-                    $key['start'] = strpos($psql_row[18], "/* AUTH: ") + 3;
-                    $key['end'] = strpos($psql_row[18], " */");
-                    $auth = " | " . substr($psql_row[18], $key['start'], $key['end'] - $key['start']);
+                $key = array();
+                if (preg_match("~/\* AUTH: \"[\w\-]+\" \*/~", $psql_row[19]) === 1) {
+                    $key['start'] = strpos($psql_row[self::CONSTANTS['psql_data_auth']], "/* AUTH: ") + 3;
+                    $key['end'] = strpos($psql_row[self::CONSTANTS['psql_data_auth']], " */");
+                    $auth = " | " . substr($psql_row[self::CONSTANTS['psql_data_auth']], $key['start'], $key['end'] - $key['start']);
                 } else {
                     $auth = " | AUTH NOT LOGGED";
                 }
 
                 //Get preferred name change tokens.
                 $preferred_name = array();
-                $preferred_names_data = explode(" ", $psql_row[14]);
+                $preferred_names_data = explode(" ", $psql_row[self::CONSTANTS['psql_data_pfn']]);
                 $key = array_search("PREFERRED_FIRSTNAME", $preferred_names_data);
                 if ($key !== false) {
                     $preferred_name['first']['old'] = $preferred_names_data[$key+2];
@@ -135,13 +147,13 @@ class main {
                 //Build preferred name change log entry.
                 $submitty_log = $date . $auth;
                 if (isset($preferred_name['first'])) {
-                    $submitty_log .= " | PREFERRED_FIRSTNAME OLD: {$preferred_name['first']['old']} -> NEW: {$preferred_name['first']['new']}";
+                    $submitty_log .= " | PREFERRED_FIRSTNAME OLD: {$preferred_name['first']['old']} NEW: {$preferred_name['first']['new']}";
                 } else {
                     $submitty_log .= " | PREFERRED_FIRSTNAME UNCHANGED";
                 }
 
                 if (isset($preferred_name['last'])) {
-                    $submitty_log .= " | PREFERRED_LASTNAME OLD: {$preferred_name['last']['old']} -> NEW: {$preferred_name['last']['new']}";
+                    $submitty_log .= " | PREFERRED_LASTNAME OLD: {$preferred_name['last']['old']} NEW: {$preferred_name['last']['new']}";
                 } else {
                     $submitty_log .= " | PREFERRED_LASTNAME UNCHANGED";
                 }
